88f2d57aa18a8a225960e2f4a8c45597d8e412f6
Naveen Singh <snaveen935@gmail.com>
2024-11-14 23:45:02 +0530

    Enable resource shrinking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ android {
         }
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
