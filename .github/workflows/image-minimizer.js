1b1867bb4e518decd07ddc2ddc03743d641259ff
Naveen Singh <snaveen935@gmail.com>
2024-11-17 22:42:38 +0530

    Update image minimizer

--- .github/workflows/image-minimizer.js
+++ .github/workflows/image-minimizer.js
@@ -139,7 +139,6 @@ module.exports = async ({github, context}) => {
             if (shouldModify) {
                 wasMatchModified = true;
                 console.log(`Modifying match '${match}'`);
-                var newBody
                 return `<img alt="${g1}" src="${g2}" width=${Math.min(600, Math.floor(IMG_MAX_HEIGHT_PX * probeAspectRatio))} />`;
             }
 
