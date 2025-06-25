df91bbc7593b401b9b1a30650a6def5e4c34bbb7
Naveen Singh <snaveen935@gmail.com>
2025-06-25 15:30:46 +0530

    fix(i18n): generate locale config automatically
    One can be maintained in-app if missing translations become a problem.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,6 +102,11 @@ android {
         includeInApk = false
     }
 
+    androidResources {
+        @Suppress("UnstableApiUsage")
+        generateLocaleConfig = true
+    }
+
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = project.libs.versions.app.build.kotlinJVMTarget.get()
     }
