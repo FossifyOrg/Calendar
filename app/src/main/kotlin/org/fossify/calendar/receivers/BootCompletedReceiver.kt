15b08214531b66d61ae039bb38b6538de00a23ad
Naveen Singh <snaveen935@gmail.com>
2025-08-29 16:18:49 +0530

    fix: call `goAsync()` before doing background work in boot completed receiver
    It's the recommended mechanism. Otherwise, there's a good chance that the system may kill the process as soon as `onReceive()` returns.

--- app/src/main/kotlin/org/fossify/calendar/receivers/BootCompletedReceiver.kt
+++ app/src/main/kotlin/org/fossify/calendar/receivers/BootCompletedReceiver.kt
@@ -1,14 +1,21 @@
 package org.fossify.calendar.receivers
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import org.fossify.calendar.extensions.*
+import org.fossify.calendar.extensions.checkAndBackupEventsOnBoot
+import org.fossify.calendar.extensions.notifyRunningEvents
+import org.fossify.calendar.extensions.recheckCalDAVCalendars
+import org.fossify.calendar.extensions.scheduleAllEvents
+import org.fossify.calendar.extensions.scheduleNextAutomaticBackup
 import org.fossify.commons.helpers.ensureBackgroundThread
 
 class BootCompletedReceiver : BroadcastReceiver() {
 
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context, intent: Intent) {
+        val pendingResult = goAsync()
         ensureBackgroundThread {
             context.apply {
                 scheduleAllEvents()
@@ -16,6 +23,7 @@ class BootCompletedReceiver : BroadcastReceiver() {
                 recheckCalDAVCalendars(true) {}
                 scheduleNextAutomaticBackup()
                 checkAndBackupEventsOnBoot()
+                pendingResult.finish()
             }
         }
     }
